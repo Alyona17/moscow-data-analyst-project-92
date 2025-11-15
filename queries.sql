38c8b306fbfe3ffc3722d46999abf6537221f35d
Alyona17 <mirror16@rambler.ru>
2025-11-15 20:47:06 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -56,3 +56,60 @@ group by seller, -- группируем по продавцу
 extract(isodow from s.sale_date), -- группируем по ISO-номеру дня недели
 day_of_week -- группируем по названию дня недели
 order by extract(isodow from s.sale_date), seller; -- сортируем по номеру дня недели, продавцу
+
+
+
+
+
+-- шаг 6 таблица 1
+SELECT
+CASE
+WHEN age BETWEEN 16 AND 25 THEN '16-25'
+WHEN age BETWEEN 26 AND 40 THEN '26-40'
+ELSE '40+'
+END AS age_category, -- разделяем покупателей на возрастные категории
+COUNT(*) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category;
+
+
+-- шаг 6 таблица 2
+SELECT
+to_char(s.sale_date, 'YYYY-MM') as selling_month, -- преобразуем дату в нужный нам формат
+count(distinct(s.customer_id)) as total_customers, -- подсчитываем количество уникальных покупателей, группируем по месяцам
+round(sum(s.quantity*p.price), 0) as income
+FROM sales s
+inner join products p
+on s.product_id = p.product_id
+GROUP BY selling_month
+ORDER BY selling_month;
+
+
+--шаг 6 таблица 3
+with first_sales as (
+select
+customer_id,
+min(sale_date) as first_sale_date -- находим первую дату покупки для каждого покупателя
+from sales
+group by customer_id
+),
+promotional_first_sales as ( -- из первого подзапроса отбираем только те первые покупки, где товар был акционным (цена = 0)
+select 
+fs.customer_id,
+fs.first_sale_date,
+s.sales_person_id,
+s.product_id
+from first_sales fs
+inner join sales s on fs.customer_id = s.customer_id 
+and fs.first_sale_date = s.sale_date
+inner join products p on s.product_id = p.product_id
+where p.price = 0)
+select
+concat(c.first_name, ' ', c.last_name) as customer,
+pfs.first_sale_date as sale_date,
+concat(e.first_name, ' ', e.last_name) as seller
+from promotional_first_sales pfs
+inner join customers c on pfs.customer_id = c.customer_id -- соединяем результаты из второго подзапроса с таблицами customers и employees, чтобы получить имена покупателей и продавцов
+inner join employees e on pfs.sales_person_id = e.employee_id
+order by c.customer_id asc;
